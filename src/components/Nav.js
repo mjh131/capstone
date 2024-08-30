9881d650106df983f80c5b258018996f25971783
mjh131 <72174676+mjh131@users.noreply.github.com>
2024-08-29 19:30:27 -0500

    Updated anchor tages to Links

---
+++ src/components/Nav.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import logo from "../images/Logo .svg";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+      <Link to="/" className="logo">
+        <img src={logo} alt="" />
+      </Link>
+      <div className="menu-icon" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/">About</Link>
+        </li>
+        <li>
+          <Link to="/">Services</Link>
+        </li>
+        <li>
+          <Link to="/menu">Menu</Link>
+        </li>
+        <li>
+          <Link to="/booking">Reservations</Link>
+        </li>
+        <li>
+          <Link to="/">Order Online</Link>
+        </li>
+        <li>
+          <Link to="/">Login</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
