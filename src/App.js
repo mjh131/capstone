ef73453f0c62ef3ef2051c5d8d18a533a879df73
mjh131 <72174676+mjh131@users.noreply.github.com>
2024-08-29 19:40:33 -0500

    Updated Menu

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import './App.css';
-import Nav from "./components/Nav"
-import Main from "./components/Main"
-import Menu from './components/Menu';
-import Footer from './components/Footer';
-
+import React from "react";
+import "./App.css";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Menu from "./components/Menu";
+import Footer from "./components/Footer";
 
 function App() {
   return (
- 
     <>
-      <Nav/>
+      <Nav />
       <Main />
-      <Menu/>
-      <Footer/>
+      <Menu />
+      <Footer />
     </>
   );
 }
